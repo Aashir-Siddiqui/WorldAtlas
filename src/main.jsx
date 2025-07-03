8350360214c4299a054e10a38076f3230600a0b9
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-07-03 05:46:14 +0500

    Update routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
-import React from "react";
-import { StrictMode } from 'react';
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const root = document.getElementById("root");
-
-ReactDOM.createRoot(root).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
