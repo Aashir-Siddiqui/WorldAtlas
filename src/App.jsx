48782edf5b6eab46ee7c8ca24e47d0f50a6044a8
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-07-06 03:31:02 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
-import React from "react";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Country from "./components/pages/Country";
-import Contact from "./components/pages/Contact";
-import Error from "./components/pages/Error";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import AppLayout from "./components/layout/AppLayout";
+// App.jsx
+import React from 'react';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Country from './components/pages/Country';
+import Contact from './components/pages/Contact';
+import Error from './components/pages/Error';
+import { Route, Routes } from 'react-router-dom';
+import AppLayout from './components/layout/AppLayout';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <AppLayout />,
-      errorElement: <Error />,
-      children: [
-        { path: "/", element: <Home /> },
-        { path: "/about", element: <About /> },
-        { path: "/country", element: <Country /> },
-        { path: "/contact", element: <Contact /> },
-      ],
-    },
-    { path: "*", element: <Error /> },
-  ]);
-
-  return <RouterProvider router={router} />
+  return (
+    <Routes>
+      <Route element={<AppLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="country" element={<Country />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
 }
 
 export default App;
