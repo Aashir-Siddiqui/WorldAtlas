f75db9694963d279d1d3ad2cbb23e0f3b35e4169
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-07-08 03:03:30 +0500

    Updated

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="relative flex h-full w-full overflow-hidden bg-black antialiased md:items-center md:justify-center">
         <Spotlight />
         <div className="p-5 w-full text-center relative z-10 max-w-3xl sm:max-w-4xl md:max-w-7xl mx-auto">
-          <h1 className="text-neutral-300 text-[28px] md:text-7xl mt-40 font-bold flex items-center justify-center gap-1 md:gap-3">
+          <h1 className="text-neutral-300 text-[28px] md:text-7xl mt-40 sm:mt-64 font-bold flex items-center justify-center gap-1 md:gap-3">
             EXPLORE <PointerHighlight><span>WORLDATLAS</span></PointerHighlight>
           </h1>
           <p className='text-neutral-300 text-sm leading-6 sm:text-lg my-10 font-normal text-center'>Discover the world like never before with WorldAtlas, your ultimate guide to global geography and culture. Explore detailed information about every country, from vibrant capitals to unique traditions, all presented in an easy-to-navigate format. Whether you're a traveler, student, or curious explorer, WorldAtlas brings the world to your fingertips with stunning visuals and up-to-date data.</p>
@@ -25,7 +25,7 @@ export default function Home() {
           </NavLink>
         </div>
       </div>
-      <div className='bg-black py-40'>
+      <div className='bg-black py-40 sm:py-60'>
         <CardHoverEffectDemo />
       </div>
     </>
